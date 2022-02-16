879e08e259be7b97337e299d509cb7306e6c332d
rkjin <bjkim3333@gmail.com>
2022-02-16 11:18:13 +0900

    j4th

--- scripts/bjnet_fulsed_sceneflow.sh
+++ scripts/bjnet_fulsed_sceneflow.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -x
-DATAPATH="/content/CFNet/datasets/kitti12_15/"
+DATAPATH="/content/BJNet/datasets/kitti12_15/"
 CUDA_VISIBLE_DEVICES=1 python main.py --dataset kitti \
     --datapath $DATAPATH --trainlist ./filenames/kitticombine.txt --testlist ./filenames/kitticombine_val.txt \
     --epochs 20 --lr 0.001 --lrepochs "12,16,18,20:2" --batch_size 1 --maxdisp 256 \
-    --model fused --logdir ./CFNet/logdir  \
+    --model fused --logdir ./BJNet/logdir  \
     --test_batch_size 1
