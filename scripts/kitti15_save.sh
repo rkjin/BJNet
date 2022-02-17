7548b9542b0033c70c2f19e64146bc9961dffbb9
rkjin <bjkim3333@gmail.com>
2022-02-17 23:40:44 +0900

    home

--- scripts/kitti15_save.sh
+++ scripts/kitti15_save.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
-DATAPATH="/content/CFNet/datasets/kitti12_15/Kitti/"
-CUDA_VISIBLE_DEVICES=0 python save_disp.py --datapath $DATAPATH --testlist ./filenames/kitti15_test.txt --model cfnet --maxdisp 256 \
---loadckpt "/content/CFNet/datasets/finetuning_model"
+DATAPATH="/content/BJNet/datasets/kitti12_15/Kitti/"
+CUDA_VISIBLE_DEVICES=0 python save_disp.py --datapath $DATAPATH --testlist ./filenames/kitti15_test.txt --model fused --maxdisp 256 \
+--loadckpt "/content/drive/MyDrive/logdir/checkpoint_000397.ckpt"
