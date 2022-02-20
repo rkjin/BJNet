fa14f0e95daca64ad0fb37818b23652ccc2369e3
rkjin <bjkim3333@gmail.com>
2022-02-20 12:13:11 +0900

    2nd

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from models.bjnet_fused_old  import BJNet
+from models.bjnet_fused_1st  import BJNet
 from models.loss import model_loss
 
 __models__ = {
--- models/cfnet_reference.py
+++ models/cfnet_reference.py
@@ -541,10 +541,7 @@ class cfnet(nn.Module):
         pred2_s4_cur = pred2_s4.detach()#[1, 1, 32, 64])
         pred2_v_s4 = disparity_variance(pred2_possibility_s4, self.maxdisp // 8, pred2_s4_cur)  # get the variance[1, 1, 32, 64])
         pred2_v_s4 = pred2_v_s4.sqrt()#[1, 1, 32, 64])
-        mindisparity_s3 = pred2_s4_cur - (self.gamma_s3 + 1) * pred2_v_s4 - self.beta_s3#[1, 1, 32, 64]) #torch.Size([1]) torch.Size([1])
-        maxdisparity_s3 = pred2_s4_cur + (self.gamma_s3 + 1) * pred2_v_s4 + self.beta_s3#[1, 1, 32, 64])
-        maxdisparity_s3 = F.upsample(maxdisparity_s3 * 2, [left.size()[2] // 4, left.size()[3] // 4], mode='bilinear', 
-                                    align_corners=True) #[1, 1, 64, 128])
+        mindisparity_s3 = pred2_s4_cur - disparity_regression_corners=True) #[1, 1, 64, 128])
         mindisparity_s3 = F.upsample(mindisparity_s3 * 2, [left.size()[2] // 4, left.size()[3] // 4], mode='bilinear',
                                     align_corners=True)#[1, 1, 64, 128])
 
