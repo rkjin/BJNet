de3ce6b855658668f156ad3ca7db643fd2b3dce2
rkjin <bjkim3333@gmail.com>
2022-03-09 23:22:57 +0900

    0309

--- scripts/kitti15_save.sh
+++ scripts/kitti15_save.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
 DATAPATH="/content/drive/MyDrive/data/kitti12_15/Kitti/"
-CUDA_VISIBLE_DEVICES=0 python save_disp.py --datapath $DATAPATH --testlist ./filenames/kitti15_test.txt --model CFNet_modified --maxdisp 256 \
+CUDA_VISIBLE_DEVICES=0 python save_disp.py --datapath $DATAPATH --testlist ./filenames/kitti15_test.txt --model cfnet_modified --maxdisp 256 \
 --loadckpt "/content/drive/MyDrive/data/finetuning_model"
