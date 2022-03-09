53d37debd1e8f15b2770fb108d5f055926936598
rkjin <bjkim3333@gmail.com>
2022-03-09 23:47:15 +0900

    0309

--- scripts/cfnet_4th_sceneflow.sh
+++ scripts/cfnet_4th_sceneflow.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -x
-DATAPATH="/content/drive/MyDrive/data/kitti12_15/Kitti/"
+DATAPATH="/content/drive/MyDrive/data/kitti12_15/"
 CUDA_VISIBLE_DEVICES=0 python main.py --dataset kitti \
     --datapath $DATAPATH --trainlist ./filenames/kitticombine.txt --testlist ./filenames/kitticombine_val.txt \
     --epochs 400 --lr 0.001 --lrepochs "12,16,18,20:2" --batch_size 1 --maxdisp 256 \
     --model cfnet_modified --logdir /content/drive/MyDrive/logdir \
-    --test_batch_size 1 --resume 
+    --test_batch_size 1  
