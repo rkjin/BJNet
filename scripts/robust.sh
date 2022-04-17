b3b265bbe2c89631b9dd36e76f729d4829d7cfd4
rkjin <bjkim3333@gmail.com>
2022-04-17 15:46:16 +0900

    0417

--- scripts/robust.sh
+++ scripts/robust.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -x
-DATAPATH="/content/CFNet/datasets/kitti12_15/"
+DATAPATH="/content/drive/MyDrive/data/kitti12_15"
 CUDA_VISIBLE_DEVICES=0 python robust.py --dataset kitti \
     --datapath $DATAPATH --trainlist ./filenames/kitticombine.txt --batch_size 1 --test_batch_size 1 \
     --testlist ./filenames/kitticombine_val.txt --maxdisp 256 \
     --epochs 400 --lr 0.001  --lrepochs "300:10" \
-    --loadckpt "/content/CFNet/datasets/sceneflow_pretraining.ckpt" \
-    --resume --model cfnet --logdir ./CFNet/logdir
+    --loadckpt "/content/drive/MyDrive/data/sceneflow_pretraining.ckpt" \
+    --model cfnet --logdir ./CFNet/logdir
