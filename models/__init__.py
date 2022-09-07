32916a133a5866b1b22cc4f9a3a0a35db76053c4
rkjin <bjkim3333@gmail.com>
2022-09-07 11:31:56 +0900

    0907

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,6 @@
-from models.cfnet_4th import CFNet_modified
-from models.cfnet import CFNet 
+from models.cfnet0907 import CFNet 
 from models.loss import model_loss
 
 __models__ = {
-    "cfnet_modified": CFNet_modified,
-    "cascade": CFNet_modified,
     "cfnet":CFNet
 }
