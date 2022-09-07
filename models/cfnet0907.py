0937f5a47a1bcc2608b15b553ce978f43083b1cd
rkjin <bjkim3333@gmail.com>
2022-09-07 11:31:59 +0900

    0907

--- models/cfnet0907.py
+++ models/cfnet0907.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.utils.data
 from torch.autograd import Variable
 import torch.nn.functional as F
-from models.submodule0907 import *
+from models.submodule import *
 import math
 
 
